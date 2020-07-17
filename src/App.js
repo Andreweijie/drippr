0da125da6b8edcc21f26c8142d3a7d2a6f844d0d
Andre Goh <andregoh1996@gmail.com>
2020-07-17 18:58:17 +0800

    update

--- src/App.js
+++ src/App.js
@@ -3,23 +3,27 @@ import "./App.css";
 import Dashboard from "./components/Dashboard";
 import NavBar from "./components/NavBar";
 import firebase, { auth, provider } from "./components/firebase/firebase";
+import { Route } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
+import Login from "./components/Login";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       username: "",
-      user: null // <-- add this line
+      user: null, // <-- add this line
     };
     this.login = this.login.bind(this); // <-- add this line
     this.logout = this.logout.bind(this); // <-- add this line
   }
   componentDidMount() {
-    auth.onAuthStateChanged(user => {
+    auth.onAuthStateChanged((user) => {
       if (user) {
         this.setState({ user });
       }
     });
+    this.logout();
   }
   handleChange(e) {
     /* ... */
@@ -28,15 +32,15 @@ class App extends Component {
     // we will add the code for this in a moment, but need to add the method now or the bind will throw an error
     auth.signOut().then(() => {
       this.setState({
-        user: null
+        user: null,
       });
     });
   }
   login() {
-    auth.signInWithPopup(provider).then(result => {
+    auth.signInWithPopup(provider).then((result) => {
       const user = result.user;
       this.setState({
-        user
+        user,
       });
     });
   }
@@ -44,10 +48,12 @@ class App extends Component {
     return (
       <div className="App">
         <NavBar></NavBar>
-        <Dashboard user={this.state.user}></Dashboard>
-        {this.state.user ? <h1>{this.state.user.email}</h1> : null}
-        <button onClick={this.login}>LOGIN</button>
-        <button onClick={this.logout}>LOGOUT</button>
+        <Route path="/login" component={Login}></Route>
+        <PrivateRoute
+          path="/dashboard"
+          user={this.state.user}
+          component={Dashboard}
+        />
       </div>
     );
   }
